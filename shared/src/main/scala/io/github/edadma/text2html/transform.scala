952f1116a58b7389247bc4d6829f66abc547bf2a
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-26 21:58:53 -0500

    code transform()

---
+++ shared/src/main/scala/io/github/edadma/text2html/transform.scala
@@ -0,0 +1,84 @@
+package io.github.edadma.text2html
+
+import io.github.edadma.char_reader.CharReader
+
+import scala.annotation.tailrec
+
+def transform(md: String): String =
+  @tailrec
+  def skipUntilAfter(r: CharReader, c: Char): CharReader =
+    if r.eoi then r
+    else if r.ch == c then r.next
+    else skipUntilAfter(r.next, c)
+
+  @tailrec
+  def skipSpace(r: CharReader): CharReader =
+    if r.ch == ' ' then skipSpace(r.next)
+    else r
+
+  @tailrec
+  def skipRepeating(r: CharReader, c: Char, count: Int = 0): (Int, CharReader) =
+    if r.ch == c then skipRepeating(r.next, c, count + 1)
+    else (count, r)
+
+  @tailrec
+  def consumeLine(r: CharReader, buf: StringBuilder = new StringBuilder): (String, CharReader) =
+    if r.eoi then (buf.toString, r)
+    else if r.ch == '\n' then (buf.toString, r.next)
+    else
+      buf += r.ch
+      consumeLine(r.next, buf)
+
+  val buf = new StringBuilder
+  val par = new StringBuilder
+
+  def tag(name: String, body: String): Unit =
+    if buf.nonEmpty then buf += '\n'
+
+    buf ++= s"<$name>$body</$name>"
+
+  def add(text: String): Unit =
+    if par.nonEmpty then par += '\n'
+
+    par ++= text
+
+  def paragraph(): Unit =
+    if par.nonEmpty then
+      tag("p", par.toString)
+      par.clear()
+
+  @tailrec
+  def transform(r: CharReader): String =
+    if r.eoi then
+      paragraph()
+      buf.toString
+    else if r.ch == '\n' then
+      paragraph()
+      transform(r.next)
+    else if r.ch == '#' then
+      val (count, r1) = skipRepeating(r, '#')
+      val r2 = skipSpace(r1)
+      val (heading, r3) = consumeLine(r2)
+
+      paragraph()
+      tag(s"h$count", heading)
+      transform(r3)
+    else
+      val (line, r1) = consumeLine(r)
+      val (next, r2) = consumeLine(r1)
+
+      if next.nonEmpty && next.forall(_ == '=') then
+        paragraph()
+        tag("h1", line)
+        transform(r2)
+      else if next.nonEmpty && next.forall(_ == '-') then
+        paragraph()
+        tag("h2", line)
+        transform(r2)
+      else
+        add(line)
+        transform(r1)
+  end transform
+
+  transform(CharReader.fromString(md))
+end transform
