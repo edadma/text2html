8ad7e6d53a8c7ebda15f99b712c2997387ce77e8
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-27 12:02:51 -0500

    generating output

--- shared/src/main/scala/io/github/edadma/text2html/Main.scala
+++ shared/src/main/scala/io/github/edadma/text2html/Main.scala
@@ -16,29 +16,24 @@ import scala.annotation.tailrec
 
   if (!Files.isReadable(root)) problem(s"Directory $root is unreadable")
 
-  if (!Files.exists(output))
-    println(s"Creating directory $output")
-    Files.createDirectories(output)
-
   list(root).filter(p => isDir(p) && !p.getFileName.toString.startsWith(".")) foreach { d =>
     println(s"Processing directory $d")
 
     val book = d.getFileName.toString
     val bookName = book split '_' map (_.capitalize) mkString " "
+    val outdir = output resolve book
 
-    println(s"Book '$bookName'")
+    println(s"Creating directory $outdir for book '$bookName'")
+    Files.createDirectories(outdir)
 
     list(d).filter(_.getFileName.toString.endsWith(".md")) foreach { f =>
       println(s"Reading markdown file $f")
 
       if (!Files.isReadable(f)) problem(s"File $f is unreadable")
 
-      val chapter = f.getFileName.toString.dropRight(3)
+      val chapter = f.getFileName.toString.dropRight(3).toInt.toString
       val in = Files.readString(f)
-      val outfile = output.resolve(s"$book-$chapter.html")
-
-      if (!Files.isWritable(outfile)) problem(s"File $outfile is unwritable")
-
+      val outfile = outdir resolve s"$chapter.html"
       val out = new PrintWriter(outfile.toString)
 
       println(s"Writing to file $outfile")
