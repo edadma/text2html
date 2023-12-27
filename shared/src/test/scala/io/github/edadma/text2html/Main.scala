9a0507f26c448efecb19b27c32346cbbb9007580
Edward A Maxedon Sr <edadma@gmail.com>
2023-12-26 21:53:03 -0500

    code transform()

--- shared/src/test/scala/io/github/edadma/text2html/Main.scala
+++ shared/src/test/scala/io/github/edadma/text2html/Main.scala
@@ -7,11 +7,13 @@ import scala.annotation.tailrec
 
 @main def run(): Unit =
   val input = """
-    |# testing
+    |testing
+    |=======
     |
     |asdf
     |
-    |##next heading
+    |next heading
+    |------------
     |
     |zxcv
     |dfhg
@@ -19,69 +21,3 @@ import scala.annotation.tailrec
 
   pprintln(input)
   pprintln(transform(input))
-
-def transform(md: String): String =
-  @tailrec
-  def skipUntilAfter(r: CharReader, c: Char): CharReader =
-    if r.eoi then r
-    else if r.ch == c then r.next
-    else skipUntilAfter(r.next, c)
-
-  @tailrec
-  def skipSpace(r: CharReader): CharReader =
-    if r.ch == ' ' then skipSpace(r.next)
-    else r
-
-  @tailrec
-  def skipRepeating(r: CharReader, c: Char, count: Int = 0): (Int, CharReader) =
-    if r.ch == c then skipRepeating(r.next, c, count + 1)
-    else (count, r)
-
-  @tailrec
-  def consumeLine(r: CharReader, buf: StringBuilder = new StringBuilder): (String, CharReader) =
-    if r.eoi then (buf.toString, r)
-    else if r.ch == '\n' then (buf.toString, r.next)
-    else
-      buf += r.ch
-      consumeLine(r.next, buf)
-
-  val buf = new StringBuilder
-  val par = new StringBuilder
-
-  def tag(name: String, body: String): Unit =
-    if buf.nonEmpty then buf += '\n'
-
-    buf ++= s"<$name>$body</$name>"
-
-  def paragraph(): Unit =
-    if par.nonEmpty then
-      tag("p", par.toString)
-      par.clear()
-
-  @tailrec
-  def transform(r: CharReader): String =
-    if r.eoi then
-      paragraph()
-      buf.toString
-    else if r.ch == '\n' then
-      paragraph()
-      transform(r.next)
-    else if r.ch == '#' then
-      val (count, r1) = skipRepeating(r, '#')
-      val r2 = skipSpace(r1)
-      val (heading, r3) = consumeLine(r2)
-
-      paragraph()
-      tag(s"h$count", heading)
-      transform(r3)
-    else
-      val (line, r1) = consumeLine(r)
-
-      if par.nonEmpty then par += '\n'
-
-      par ++= line
-      transform(r1)
-  end transform
-
-  transform(CharReader.fromString(md))
-end transform
